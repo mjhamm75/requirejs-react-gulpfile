6802b6279ccf7d300d10382770f9da1b1d421e22
Jason Hamm <jason.hamm@availity.com>
2014-03-09 09:55:29 -0400

    Add route/view to podcasts

--- app/js/router/app.router.js
+++ app/js/router/app.router.js
@@ -9,13 +9,15 @@ define(function(require) {
   var BooksView = require('views/books.view');
   var HomeView = require('views/home.view');
   var NavView = require('views/navbar.view');
+  var PodcastsView = require('views/podcasts.view');
 
   var Router = Backbone.Router.extend({
     routes: {
       "": "home",
+      "addBlog": "addBlog",
       "blog": "blog",
       "books": "books",
-      "addBlog": "addBlog"
+      "podcasts": "podcasts"
     },
 
     addBlog: function() {
@@ -62,6 +64,23 @@ define(function(require) {
     initialize: function() {
       var navView = new NavView();
       $('#nav').html(navView.render().el);
+    },
+
+    podcasts: function() {
+      var podcasts = [
+        {
+          "title": "Ruby Rogues"
+        },
+        {
+          "title": "Javascript jabber"
+        },
+        {
+          "title": "ETL"
+        }
+      ];
+      var podcastsCollection = new Backbone.Collection(podcasts);
+      var podcastsView = new PodcastsView(podcastsCollection);
+      this.swapView(podcastsView);
     },
 
     swapView: function(view) {
