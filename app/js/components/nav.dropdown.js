453cabf35dfda7ac33e6aa6785837b8175760521
Jason Hamm <jason.hamm@availity.com>
2014-03-12 06:17:42 -0400

    Create navbar dropdown component

--- app/js/components/nav.dropdown.js
+++ app/js/components/nav.dropdown.js
@@ -7,13 +7,17 @@ define(function(require) {
 
   var NavDropdown = React.createClass({
     render: function() {
+      var cx = React.addons.classSet;
+      var classes = cx({
+        'hidden': true
+      });
       return (
       <ul className="nav navbar-nav navbar-right">
         <li><a href="#blog">Blog</a></li>
         <li className="dropdown">
           <a href="#" className="dropdown-toggle" data-toggle="dropdown">Other <b className="caret"></b></a>
           <ul className="dropdown-menu">
-            <li><a href="#books">Books</a></li>
+            <li><a className="{classes}" href="#books">Books</a></li>
             <li><a href="#podcasts">Podcasts</a></li>
             <li><a href="#">Tech I Like</a></li>
             <li><a href="#">About me</a></li>
